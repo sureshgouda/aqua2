66328aea1b19b75e9354382589a0deb0cccc42b3
suresh <sureshgouda.h@robosoftin.com>
2018-03-19 14:15:45 +0530

    updated

--- index.js
+++ index.js
@@ -6,25 +6,34 @@ const http = require('http');
 
 const server = express();
 server.use(bodyParser.urlencoded({
-    extended: true
+  extended: true
 }));
 
 server.use(bodyParser.json());
 
-server.post("/echo", function(req, res) {
-    var speech =
-      req.body.result &&
-      req.body.result.parameters &&
-      req.body.result.parameters.echoText
-        ? req.body.result.parameters.echoText
-        : "Seems like some problem. Speak again.";
-    return res.json({
-      speech: speech,
-      displayText: speech,
-      source: "echo"
-    });
-  });
+
+
+server.post("/echo", function (req, res) {
+  var actions = req.body.result.action;
+  switch (actions) {
+    case "wbt":
+      var speech =
+        req.body.result &&
+          req.body.result.parameters &&
+          req.body.result.parameters.echoText
+          ? req.body.result.parameters.echoText
+          : "Seems like some problem. Speak again.";
+      return res.json({
+        speech: speech,
+        displayText: speech,
+        source: "echo"
+      });
+      break;
+    case "report":
+      break;
+  }
+});
 
 server.listen((process.env.PORT || 8000), function () {
-    console.log("Server is up and running...");
+  console.log("Server is up and running...");
 });

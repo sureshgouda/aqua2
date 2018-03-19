81fa8d126fdf3af5d85e2eb055807063cceeff17
suresh <sureshgouda.h@robosoftin.com>
2018-03-19 14:42:18 +0530

    updated

--- index.js
+++ index.js
@@ -30,6 +30,48 @@ server.post("/echo", function (req, res) {
       });
       break;
     case "report":
+      var name = req.body.result.parameters.name;
+      switch (name) {
+        case "Lakshmi":
+          return res.json({
+            "sequenceId": "",
+            "text": `Hello ${name}, I can see that you are new user If you are a new use compete all the Web based training in order to generate report. Click on below link for WBT.`,
+            "menu": [],
+            "link": ""
+          });
+          break;
+        case "Ralf":
+          return res.json({
+            "sequenceId": "",
+            "text": `Hello ${name}, you completed 50% of web based training. So compete all the Web based training in order to generate report. Click on below link for WBT.`,
+            "menu": [],
+            "link": ""
+          });
+          break;
+        case "Mathias":
+          return res.json({
+            "sequenceId": "",
+            "text": `AQUA offers a rich set for reports in the form of Standard and Custom reports for the End users. Please choose you intended report to see from the below list.`,
+            "menu": ["product realiability", "quick test"],
+            "link": ""
+          });
+          break;
+        case "Vivek":
+          return res.json({
+            "sequenceId": "",
+            "text": `AQUA offers a rich set for reports in the form of Standard and Custom reports for the End users. Please choose you intended report to see from the below list.`,
+            "menu": ["product realiability", "quick test"],
+            "link": ""
+          });
+          break;
+        default:
+          return res.json({
+            "sequenceId": "",
+            "text": `AQUA offers a rich set for reports in the form of Standard and Custom reports for the End users. Please choose you intended report to see from the below list.`,
+            "menu": ["product realiability", "quick test"],
+            "link": ""
+          });
+      }
       break;
   }
 });
